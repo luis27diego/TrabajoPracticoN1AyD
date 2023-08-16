31fc0de05bd86efd6e7d2955f9806b8ac61c99e0
Slimerpy <tomybrehm29@gmail.com>
2023-08-16 18:05:56 -0300

    Nodos

--- TP1_lista_doble_enlazada/desarrollo.py
+++ TP1_lista_doble_enlazada/desarrollo.py
@@ -1,1 +1,26 @@
-#perencejo 
+class Nodo:
+    def __init__(self,valor):
+        self.valor = valor 
+        self.izq = None
+        self.der = None 
+
+
+
+    def set_izq (self, value):
+
+        self.izq = value 
+
+    def set_der (self,value):
+
+        self.der = value 
+
+
+
+nodo = Nodo(5)
+
+nodo.set_der(56)
+nodo.set_izq(800)
+print(nodo.der)
+print(nodo.izq)
+
+
