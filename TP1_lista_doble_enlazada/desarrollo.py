ee07ea04fe34570403bf6f21b71e029a83f640df
luis27diego <141271239+luis27diego@users.noreply.github.com>
2023-08-21 23:38:52 -0300

    Nuevo

--- TP1_lista_doble_enlazada/desarrollo.py
+++ TP1_lista_doble_enlazada/desarrollo.py
@@ -1,26 +1,35 @@
 class Nodo:
-    def __init__(self,valor):
-        self.valor = valor 
-        self.izq = None
-        self.der = None 
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+        self.anterior = None
+
+class ListaDoblementeEnlazada:
+    def __init__(self):
+        self.primero = None
+        self.ultimo = None
+        self.size = 0
+
+    def esta_vacia(self):
+        return self.size == 0
+
+    def insertar_al_principio(self, dato):
+        nuevo_nodo = Nodo(dato)
+        if self.esta_vacia():
+            self.primero = nuevo_nodo
+            self.ultimo = nuevo_nodo
+        else:
+            nuevo_nodo.siguiente = self.primero
+            self.primero.anterior = nuevo_nodo
+            self.primero = nuevo_nodo
+        self.size += 1
 
 
 
-    def set_izq (self, value):
 
-        self.izq = value 
+listonga = ListaDoblementeEnlazada()
+listonga.insertar_al_principio(5)
 
-    def set_der (self,value):
-
-        self.der = value 
-
-
-
-nodo = Nodo(5)
-
-nodo.set_der(56)
-nodo.set_izq(800)
-print(nodo.der)
-print(nodo.izq)
-
+print(listonga.primero)
+print(listonga.ultimo)
 
