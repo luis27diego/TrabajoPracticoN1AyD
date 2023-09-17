094c7d75b57508f6d58f7182e04ca327d2e314ea
Luis Diego <yeye.rodriguez.27@gmail.com>
2023-09-17 16:58:55 -0300

    Actualizar con comentarios ahora

--- TP1_LDE_testing/ListaDobleEnLazada.py
+++ TP1_LDE_testing/ListaDobleEnLazada.py
@@ -1,105 +1,141 @@
+# Definición de la clase Nodo que representa un nodo en la lista doblemente enlazada.
 class Nodo:
     def __init__(self, dato):
         self.dato = dato
         self.siguiente = None
         self.anterior = None
 
-
+# Definición de la clase ListaDobleEnlazada que representa una lista doblemente enlazada.
 class ListaDobleEnlazada:
     def __init__(self):
+        # Inicialización de la cabeza, cola y tamaño de la lista.
         self.cabeza = None
         self.cola = None
         self.tamanio = 0
 
+# Método para hacer que la lista sea iterable.
     def __iter__(self):
         self._actual = self.cabeza
         return self
-
+    
+# Método para avanzar a través de la lista en la iteración.
     def __next__(self):
         if self._actual is None:
             raise StopIteration
         dato = self._actual.dato
         self._actual = self._actual.siguiente
         return dato
-
+    
+# Método para obtener el tamaño de la lista.
     def tamanio(self):
 
         return self.tamanio
-
+    
+# Método para verificar si la lista está vacía.
     def esta_vacia(self):
         return self.tamanio == 0
-   
+    
+# Método para agregar un nuevo nodo al principio de la lista.   
     def agregar_al_inicio(self, dato):
+
+        # Crear un nuevo nodo con el dato proporcionado.
         nuevo_nodo = Nodo(dato)
+
+        # Verificar si la lista está vacía.
         if self.esta_vacia():
+
+            # Si la lista está vacía, el nuevo nodo se convierte en la cabeza y la cola de la lista.
             self.cabeza = nuevo_nodo
             self.cola = nuevo_nodo
+
         else:
+            # Si la lista no está vacía, se enlaza el nuevo nodo al nodo actual de la cabeza.
             nuevo_nodo.siguiente = self.cabeza
             self.cabeza.anterior = nuevo_nodo
+
+            # El nuevo nodo se convierte en la nueva cabeza de la lista.
             self.cabeza = nuevo_nodo
         self.tamanio += 1
-   
+
+# Método para agregar un nuevo nodo al final de la lista.  
     def agregar_al_final(self,dato):
         nuevo_nodo = Nodo(dato)
+
+        # Verificar si la lista está vacía.
         if self.esta_vacia():
+
+            # Si la lista está vacía, el nuevo nodo se convierte en la cabeza y la cola de la lista.
             self.cabeza = nuevo_nodo
             self.cola = nuevo_nodo
+
+        # Si la lista no está vacía, se enlaza el nuevo nodo al nodo actual de la cola.
         else:
             nuevo_nodo.anterior = self.cola
             self.cola.siguiente = nuevo_nodo
+
+            # El nuevo nodo se convierte en la nueva cola de la lista.
             self.cola  = nuevo_nodo
         self.tamanio += 1
-           
+
+# Método para obtener la longitud de la lista utilizando len().           
     def __len__(self):
         return self.tamanio
 
-
-   
+# Método para insertar un nuevo nodo en una posición específica o al final de la lista.
     def insertar(self,dato,posicion = None):
         nuevo_nodo = Nodo(dato)
 
+        # Verifica si la lista está vacía.
         if not self.cabeza:
+
+            # Si la lista está vacía, el nuevo nodo se convierte en la cabeza y la cola de la lista.
             self.cabeza = nuevo_nodo
             self.cola = nuevo_nodo
 
-
+        # Si la posición no se proporciona o es mayor que el tamaño actual de la lista, el nuevo nodo se agrega al final de la lista.
         elif posicion is None or posicion >= self.tamanio:
             nuevo_nodo.anterior = self.cola
             self.cola.siguiente = nuevo_nodo
             self.cola = nuevo_nodo
 
-
+        # Si la posición es 0, el nuevo nodo se convierte en la nueva cabeza de la lista, se agrega al inicio
         elif posicion == 0:
             nuevo_nodo.siguiente = self.cabeza
             self.cabeza.anterior = nuevo_nodo
             self.cabeza = nuevo_nodo
 
-
+        # Agregar el nuevo nodo a la posición indicada 
         else:
+            # Buscamos el nodo en la posición dada.
             actual = self.cabeza
             for _ in range(posicion - 1):
                 actual = actual.siguiente
+
+            # Se acomodan los enlaces para insertar el nuevo nodo en la posición indicada.
             nuevo_nodo.siguiente = actual.siguiente
             nuevo_nodo.anterior = actual
             actual.siguiente.anterior = nuevo_nodo
             actual.siguiente = nuevo_nodo
         self.tamanio += 1
 
-
-
-
+# Método para copiar la lista en una nueva lista doblemente enlazada.
     def copiar(self):
         nuevalista = ListaDobleEnlazada()
         actual = self.cabeza
+
+        # Recorrer la lista original y agregar cada elemento a la nueva lista.
         while actual !=None:
             nuevalista.insertar(actual.dato)
+
+            # Inicializar un puntero al nodo actual, comenzando desde la cabeza de la lista original.
             actual = actual.siguiente
-       
+
+        # Devolver la nueva lista doblemente enlazada que contiene una copia de los datos.
         return nuevalista
 
-
+# Método para extraer un nodo de una posición específica o el último nodo si no se proporciona una posición.
     def extraer(self, posicion=None):
+
         # Verificar si la lista está vacía
         if self.tamanio == 0:
             raise ValueError("Extraer de una lista vacia deberia extraer error")
@@ -107,10 +143,13 @@ class ListaDobleEnlazada:
         # Si no se proporciona una posición, eliminar el último elemento
         if posicion is None or posicion == -1:
             dato = self.cola.dato
+
             # Si hay solo un nodo en la lista
             if self.cabeza == self.cola:
                 self.cabeza = None
                 self.cola = None
+
+            # El ultimo nodo de la lista
             else:
                 self.cola = self.cola.anterior
                 self.cola.siguiente = None
@@ -151,82 +190,100 @@ class ListaDobleEnlazada:
         self.tamanio -= 1
         return dato
 
-
+# Método para invertir la lista doblemente enlazada.
     def invertir(self):
         if self.esta_vacia() or self.cabeza == self.cola:
             return
    
         actual = self.cabeza
         while actual:
+            # Se invierten los enlaces de los nodos para invertir la lista.
             actual.anterior, actual.siguiente = actual.siguiente, actual.anterior
             actual = actual.anterior
-   
+
+        # Se actualiza la cabeza y la cola después de la inversión.
         self.cabeza, self.cola = self.cola, self.cabeza
 
-
+# Método para concatenar la lista actual con otra lista proporcionada.
     def concatenar(self, otra_lista):
         
         
-        lista = otra_lista.copiar()
+        lista = otra_lista.copiar()          # Copiar la lista proporcionada para evitar modificarla directamente.
+
+        # Enlazar la cola de la lista actual con la cabeza de la lista copiada.
         self.cola.siguiente = lista.cabeza
         lista.cabeza.anterior = self.cola
+
+        # Actualizar la cola de la lista actual para que sea la cola de la lista copiada.
         self.cola = lista.cola
 
         self.tamanio += lista.tamanio
-
-        
-    
+   
+# Sobrecarga del operador '+' para concatenar dos listas.   
     def __add__(self,lista):
-        lista_e = ListaDobleEnlazada()
+        lista_resultante = ListaDobleEnlazada()
         nodo_a = self.cabeza
 
+        # Copiar los elementos de la lista actual a la nueva lista resultante.
         while nodo_a != None:
-            lista_e.agregar_al_final(nodo_a.dato)
+            lista_resultante.agregar_al_final(nodo_a.dato)
             nodo_a = nodo_a.siguiente
 
-        lista_e.concatenar(lista) 
-        return lista_e
+        # Concatenar la lista proporcionada a la nueva lista resultante.
+        lista_resultante.concatenar(lista) 
+        return lista_resultante
     
+# Método para ordenar la lista utilizando el algoritmo quicksort.
     def ordenar(self):
-        # Primera llamada a otro metodo, pasandole la posicion 0 que será la cabeza del nodo
-        # y el final que sería la cola
+        
+        # Primera llamada a otro metodo, pasandole la posicion 0 que será la cabeza del nodo y el final que sería la cola
         self.ordenar_auxiliar(0,self.tamanio-1)
 
+# Método auxiliar para realizar la ordenación mediante quicksort.
     def ordenar_auxiliar(self,primero,ultimo):
         if primero < ultimo:
-            # Lo segundo es llamar a la función quick_sort que va a ordenar y retornar
-            # un punto para dividir la lista
+            # Lo segundo es llamar a la función quick_sort que va a ordenar y retornar un punto para dividir la lista
             puntoDivision = self.quick_sort(primero,ultimo)
-            # Luego de dividir la lista, se llama a si misma para repetir el proceso de puntoDivision pero en
-            # la primera mitad
+
+            # Luego de dividir la lista, se llama a si misma para repetir el proceso de puntoDivision pero en la primera mitad
             self.ordenar_auxiliar(primero,puntoDivision-1)
+
             # Por ultimo, se llama de nuevo pero se invierten los valores para que ordene la segunda mitad
             self.ordenar_auxiliar(puntoDivision+1,ultimo)
 
+# Método que realiza el particionamiento en quicksort y devuelve el punto de división.
     def quick_sort(self,primero,ultimo):
+    # Determinar los nodos pivote, izquierdo y derecho según las posiciones proporcionadas.
 
+        # Si primero y último son los valores predeterminados, se utiliza toda la lista.
         if primero == 0 and ultimo == self.tamanio-1:
             nodo_pivote = self.cabeza
             nodo_Izq = self.cabeza.siguiente
             nodo_Der = self.cola
+
         else:
+
+            # Si primero está en la primera mitad de la lista, se utiliza el nodo pivote de la izquierda.
             if primero < (self.tamanio/2):
                 nodo_pivote = self.cabeza
                 for _ in range(primero):
                     nodo_pivote = nodo_pivote.siguiente
                 nodo_Izq = nodo_pivote.siguiente
 
+            # Si primero está en la segunda mitad de la lista, se utiliza el nodo pivote de la derecha.
             else:
                 nodo_pivote = self.cola
                 for _ in range((self.tamanio - (primero + 1))):
                     nodo_pivote = nodo_pivote.anterior
                 nodo_Izq = nodo_pivote.siguiente
 
+            # Si último está en la primera mitad de la lista, se utiliza el nodo derecho de la izquierda.
             if ultimo > (self.tamanio/2):
                 nodo_Der = self.cola
                 for _ in range((self.tamanio - (ultimo + 1))):
                     nodo_Der = nodo_Der.anterior
 
+            # Si último está en la segunda mitad de la lista, se utiliza el nodo derecho de la derecha.
             else:
                 nodo_Der = self.cabeza
                 for _ in range(ultimo):
@@ -238,25 +295,32 @@ class ListaDobleEnlazada:
         hecho = False
         while not hecho:
 
+            # Avanzamos el nodo_Izq mientras sea menor o igual al nodo_pivote.
             while marcaIzq <= marcaDer and nodo_Izq.dato <= nodo_pivote.dato:
                 nodo_Izq = nodo_Izq.siguiente
                 marcaIzq += 1
+
+            # Retrocedemos el nodo_Der mientras sea mayor o igual al nodo_pivote.
             while nodo_Der.dato >= nodo_pivote.dato and marcaDer >= marcaIzq:
                 nodo_Der = nodo_Der.anterior
                 marcaDer -=1
 
+            # Si las marcas se cruzan, hemos terminado.
             if marcaDer < marcaIzq:
                 hecho = True
 
             else:
+                # Intercambiamos los datos de los nodos Izq y Der.
                 dato_Temp = nodo_Izq.dato
                 nodo_Izq.dato = nodo_Der.dato
                 nodo_Der.dato = dato_Temp
 
+        # Intercambiamos el dato del nodo_pivote con el dato del nodo_Der.
         dato_Temp = nodo_pivote.dato
         nodo_pivote.dato = nodo_Der.dato
         nodo_Der.dato = dato_Temp
 
+        # Devolvemos la posición del nodo_Der, que es el punto de división.
         return marcaDer
 
 
--- TP1_juego_guerra/pruebas/JuegoDeGuerra.py
+++ TP1_juego_guerra/pruebas/JuegoDeGuerra.py
@@ -3,52 +3,63 @@ import random
 
 valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 palos = ['♠', '♥', '♦', '♣']
-     
+
+# Clase que representa una carta en el juego de la Guerra, se crean los atributos de la carta 
 class Carta:
     def __init__(self, valor, palo):
         self.valor = valor
         self.palo = palo
         self.boca_abajo = True
-
-    def __str__(self):
-        if self.boca_abajo:
-            return 'X'
-        else:
-            return f'{self.valor}{self.palo}'
         
+    # Método especial para convertir la carta en una cadena legible.
+    def __str__(self):
+        
+        if self.boca_abajo:
+            return 'X'  # Si la carta está boca abajo, se representa con 'X'.
+        
+        else:
+            return f'{self.valor}{self.palo}' # Si no, se muestra el valor y el palo de la carta.
+        
+# Clase que representa un mazo de cartas y las distribuye a dos jugadores.       
 class Mazo:
-    def __init__(self,semilla=190):
+    def __init__(self,semilla=None):
         self.mazo = []
-        self.jugador_1 = ListaDobleEnlazada()
-        self.jugador_2 = ListaDobleEnlazada()
+        self.jugador_1 = ListaDobleEnlazada()   # Mazo del jugador 1.
+        self.jugador_2 = ListaDobleEnlazada()   # Mazo del jugador 2.
 
+        # Creación del mazo de cartas, combinando valores y palos.
         for valor in valores:
             for palo in palos:
                 self.mazo.append(Carta(valor,palo))
 
         if semilla is not None:
-            random.seed(semilla)
+            random.seed(semilla)    # Inicialización de la semilla para la aleatoriedad.
 
-        self.barejeador()
+        self.barejeador()        # Barajar el mazo.
 
+    # Método para barajar el mazo de cartas.
     def barejeador(self):
 
+        # Verificar si el mazo no está vacío antes de barajar.
         if self.mazo:
+
+            # Se barajea el mazo.
             self.mazo = random.sample(self.mazo, len(self.mazo))
 
 
-        #random.shuffle(self.mazo)
-        
+        # Método para pasar de una lista de python a una ListaDobleEnlazada
         def pasador(self):
-            lista = ListaDobleEnlazada()
+            
+            lista = ListaDobleEnlazada()   # Se crea una ListaDobleEnlazada
             for carta in self.mazo:
-                lista.agregar_al_final(carta)
-            self.mazo = lista
+                lista.agregar_al_final(carta)   # Se agregan las cartas a ListaDobleEnlazada
+            self.mazo = lista    # Ahora self.mazo va a ser una ListaDobleEnlazada
 
         pasador(self)
-        
+
+        # Método para repartir las cartas a los dos jugadores.    
         def repartir(self):
-            for _ in range(26):
+            for _ in range(26):    # For para extraer las cartas del mazo, repartirlas y agregarlas a los jugadores.
                 carta1 = self.mazo.extraer(0)
                 carta2 = self.mazo.extraer(0)
                 self.jugador_1.agregar_al_inicio(carta1)
@@ -56,147 +67,174 @@ class Mazo:
 
         repartir(self)
         
-
-    
+    # Método para poner en la parte superior del mazo.   
     def poner_arriba(self,Carta):
         self.mazo.agregar_al_inicio(Carta)
 
-
+    # Método para poner abajo del mazo.  
     def poner_abajo(self, carta):
         self.mazo.agregar_al_final(carta)
 
-
+    # Método para sacar cartas en la parte superior del mazo.   
     def sacar_arriba(self):
         if self.mazo:
             return self.mazo.extraer(0)
         else:
             return None
-
+        
+# Clase que representa el juego de la Guerra.
 class JuegoGuerra:
     def __init__(self, semilla=None):
         self.turnos = 0
         self.max_turnos = 10000
-        self.mazo = Mazo()  # Crear una instancia de Mazo para obtener las listas de jugadores
+        self.mazo = Mazo(semilla)  # Crear una instancia de Mazo para crear el mazo y repartir las cartas a los jugadores
         self.mazo_jugador1 = self.mazo.jugador_1
         self.mazo_jugador2 = self.mazo.jugador_2
-        self.mesa = ListaDobleEnlazada()
-        random.seed(semilla)
+        self.mesa = ListaDobleEnlazada()   # Se crea la mesa
 
-
+    # Método para jugar un turno del juego.
     def jugar_turno(self):
+        
+        # Si la cantidad de turnos jugadores supera el limite en este caso 10000 termina en empate
         if self.turnos >= self.max_turnos:
             print("La partida ha terminado en empate.")
             return
+        
+        # Si el jugador 1 se queda sin cartas el jugador 2 gana
         if self.mazo_jugador1.esta_vacia(): 
             print("jugador 2 ganaa")
             return
+        
+        # Si el jugador 2 se queda sin cartas el jugador 1 gana
         if self.mazo_jugador2.esta_vacia(): 
             print("jugador 1 ganaa")
             return
-
+        
+        # Extraemos las cartas de la parte superior de los mazos de los jugadores
         carta_jugador1 = self.mazo_jugador1.extraer(0)
         carta_jugador2 = self.mazo_jugador2.extraer(0)
 
+        # Revelamos las cartas, estableciendo 'boca_abajo' en False
         carta_jugador1.boca_abajo = False
         carta_jugador2.boca_abajo = False
 
 
-
+        # Mostramos información sobre el turno actual
         print(f'Turno {self.turnos + 1}:')
         print(f'jugador1: {" ".join(str(carta) if not carta.boca_abajo else "-X" for carta in self.mazo_jugador1)}')
         print(f'jugador2: {" ".join(str(carta) if not carta.boca_abajo else "-X" for carta in self.mazo_jugador2)}')
         print(f'Carta jugador1: {carta_jugador1}')
         print(f'Carta jugador2: {carta_jugador2}') 
 
-
+        # Volvemos a ocultar las cartas
         carta_jugador1.boca_abajo = True
         carta_jugador2.boca_abajo = True
 
-
+        # Agregamos las cartas a la mesa
         self.mesa.agregar_al_final(carta_jugador1)
         self.mesa.agregar_al_final(carta_jugador2)
 
         if valores.index(carta_jugador1.valor) > valores.index(carta_jugador2.valor):
+            # Jugador 1 gana el turno
             print('Jugador 1 gana el turno.')
             while not self.mesa.esta_vacia():
                 self.mazo_jugador1.agregar_al_final(self.mesa.extraer(0))
+
         elif valores.index(carta_jugador1.valor) < valores.index(carta_jugador2.valor):
+            # Jugador 2 gana el turno
             print('Jugador 2 gana el turno.')
             while not self.mesa.esta_vacia():
                 self.mazo_jugador2.agregar_al_final(self.mesa.extraer(0))
         else:
+            # Empieza una guerra si las cartas tienen el mismo valor
             print('¡Guerra!')
             self.guerra()
 
+        # Ocultamos todas las cartas en la mesa nuevamente
         for carta in self.mesa:
             carta.boca_abajo = True
 
         self.turnos += 1    
 
+    # Método para la guerra durante el juego.
     def guerra(self):
+
+        # Si la cantidad de turnos jugadores supera el limite en este caso 10000 termina en empate
         if self.turnos >= self.max_turnos:
             print("La partida ha terminado en empate.")
             return
 
-        #guerra_mesa = ListaDobleEnlazada()
-
+        # For para extraer 3 cartas de cada jugador y agregarlas a la mesa
         for _ in range(3):
             if self.mazo_jugador1.esta_vacia(): 
+                # Si el jugador 1 se queda sin mazo, el jugador 2 gana la guerra
+
                 print("jugador 2 gana")
                 return
+            
             if self.mazo_jugador2.esta_vacia(): 
+                # Si el jugador 2 se queda sin mazo, el jugador 1 gana la guerra
                 print("jugador 1 gana")
                 return 
+            
+            # Extraemos las cartas de la parte superior de los mazos de los jugadores
             carta_jugador1 = self.mazo_jugador1.extraer(0)
             carta_jugador2 = self.mazo_jugador2.extraer(0)
 
-
-
-
-
+            # Agregamos las cartas a la mesa
             self.mesa.agregar_al_final(carta_jugador1)
             self.mesa.agregar_al_final(carta_jugador2)
 
+        # Mostramos las cartas en la mesa durante la guerra
         print('Cartas en la mesa durante la guerra:')
         for carta in self.mesa:
             print(f'{carta}', end=' ')
         print()
     
         if self.mazo_jugador1.esta_vacia(): 
+            # Si el jugador 1 se queda sin cartas durante la guerra, el jugador 2 gana la guerra
             print("jugador 2 gana")
             return
+        
         if self.mazo_jugador2.esta_vacia(): 
+            # Si el jugador 2 se queda sin cartas durante la guerra, el jugador 1 gana la guerra
             print("jugador 1 gana")
             return    
-
-
-
+        
+        # Extraemos una carta adicional de cada jugador para luego comparar
         carta_jugador1 = self.mazo_jugador1.extraer(0)
         carta_jugador2 = self.mazo_jugador2.extraer(0)
 
+        # Revelamos las cartas 
         carta_jugador1.boca_abajo = False
         carta_jugador2.boca_abajo = False
 
-
+        # Mostramos las cartas de los jugadores
         print(f'Quinta carta de Jugador 1: {carta_jugador1}')
         print(f'Quinta carta de Jugador 2: {carta_jugador2}')
 
+        # Volvemos a ocultar las cartas adicionales
         carta_jugador1.boca_abajo = True
         carta_jugador2.boca_abajo = True
 
+        # Agregamos las cartas adicionales a la mesa
         self.mesa.agregar_al_final(carta_jugador1)
         self.mesa.agregar_al_final(carta_jugador2)
 
-
         if valores.index(carta_jugador1.valor) > valores.index(carta_jugador2.valor):
+            # Jugador 1 gana la guerra
             print('Jugador 1 gana la guerra.')
             while not self.mesa.esta_vacia():
                 self.mazo_jugador1.agregar_al_final(self.mesa.extraer())
+
         elif valores.index(carta_jugador1.valor) < valores.index(carta_jugador2.valor):
+            # Jugador 2 gana la guerra
             print('Jugador 2 gana la guerra.')
             while not self.mesa.esta_vacia():
                 self.mazo_jugador2.agregar_al_final(self.mesa.extraer())
+
         else:
+            # Se produce otra guerra si las cartas tienen el mismo valor
             print('¡Guerra de nuevo!')
             if self.mazo_jugador1.esta_vacia or self.mazo_jugador2.esta_vacia:
                 return
@@ -205,12 +243,18 @@ class JuegoGuerra:
 
         self.turnos += 1
 
+    # Método para jugar una partida.
     def jugar(self):
+
+        # Mientras la cantidad de turnos no supere a la cantidad no supere a la cantidad de maxima de turnos se sigue jugando
         while self.turnos < self.max_turnos :
+
+            # Si un jugador se queda sin mazo return 
             if self.mazo_jugador1.esta_vacia() or self.mazo_jugador2.esta_vacia():
                 return
             self.jugar_turno()
 
+        # Comprobar el resultado de la partida al final.
         if self.mazo_jugador1.esta_vacia():
             print("¡Jugador 2 gana la partida!")
         elif self.mazo_jugador2.esta_vacia():
@@ -218,5 +262,5 @@ class JuegoGuerra:
         else:
             print("La partida ha terminado en empate.")
 
-juego = JuegoGuerra()
+juego = JuegoGuerra(178)
 juego.jugar()
