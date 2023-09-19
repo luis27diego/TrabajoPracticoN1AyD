56439aa11950a43f175a02649e6a4e04af7d840d
Luis Diego <yeye.rodriguez.27@gmail.com>
2023-09-18 21:06:47 -0300

    Version Final

--- TP1_juego_guerra/aplicaciones/main.py
+++ TP1_juego_guerra/aplicaciones/main.py
@@ -1,1 +1,9 @@
-# holaa
+# Archivo principal (main.py)
+from TP1_juego_guerra.modulos.JuegoDeGuerra import JuegoGuerra
+
+def main():
+    juego = JuegoGuerra(178)
+    juego.jugar()
+
+if __name__ == "__main__":
+    main()
